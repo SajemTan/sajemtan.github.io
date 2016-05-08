4a29fd23ac8eec86f5ac39e80602bbae3dbc84b2
mr-martian <popcorn.tomato.dude@gmail.com>
2016-05-07 21:32:27 -0500

    Create lexicon.js

---
+++ lexicon.js
@@ -0,0 +1,377 @@
+var lexicon = {
+  "adj": {
+    "femek": ["noetic"],
+    "jelk": ["full"],
+    "met": ["falling", "collapsing", "tumbling (chaotic falling, like an stone bouncing many times as it falls down a hill, or a vase breaking when it hits after a simple ballistic fall)"],
+    "nymem": ["clumsy"],
+    "sajem": ["common", "communal", "public"],
+    "simem": ["silent", "quiet", "calm", "sleepy", ["see also", "thneduht"]],
+    "slik": ["correct"],
+    "slnuhm": ["gray", "grey", "dull", "indeterminate", "incomplete", "boring", "plain", "numinous"],
+    "solm": ["fast", "swift", "quick (of moving objects)", ["example", "the feathered one is *solm*"], ["see also", "theln"]],
+    "suhzhnolm": ["pleasant"],
+    "theln": ["fast", "quick (or processes/actions other than motion verbs)", ["example", "*Kizhult* coins words *theln*"], ["see also", "solm"]],
+    "vynit": ["long (time)", "slow", "delayed"],
+    "zlislym": ["creepy"],
+    "znim": ["falling"]
+  },
+  "adv": {
+    "evidential": {
+      "femektholk": ["discovered by telepathy"],
+      "kic": ["carefully discerned through protracted study"],
+      "shan": ["hearsay via a living person"],
+      "slet": ["traditional knowledge", "lore"],
+      "sluln": ["based on faith/beliefs/subjective experiences"],
+      "snat": ["learned via seconday/tertiary/etc. sources, especially writings"],
+      "thec": ["deduced from sensory evidence"],
+      "thef": [["archaic", "thec"], "deduced from sensory evidence"],
+      "tholk": ["direct sensory evidence"],
+      "voln": ["understood through the reversal of a previously deep belief"]
+    },
+    "nacen": ["also"],
+    "theln": ["quickly"]
+  },
+  "aux": {
+    "i": {
+      "dic": [["form", "d", "c"], "indicative"],
+      "jit": [["form", "j", "t"], "must", "vital", "necessary"],
+      "shik": [["form", "sh", "k"], "be able to"]
+    },
+    "ol": {
+      "golm": [["form", "g", "m"], "be obligated to"],
+      "molt": [["form", "m", "t"], "not"]
+    }
+  },
+  "conjunction": {
+    "ke": [["archaic", "keh"], "and"],
+    "keh": ["and"]
+  },
+  "interjection": {
+    "slik": ["yes", "okay"]
+  },
+  "noun": {
+    "cejym": ["OCD"],
+    "cuhc": ["foot"],
+    "culjim": ["ant"],
+    "din": ["home", "hive"],
+    "divolm": ["thunder", "phonology"],
+    "duhdem": ["dam", "swamp"],
+    "femek": ["mind", "thought"],
+    "fijyc": ["rainbow"],
+    "gajin": ["pure awe"],
+    "gamyk": ["tree", "one that produces"],
+    "gigim": ["flipper", "fin"],
+    "gyn": ["worker"],
+    "jafin": ["melodrama"],
+    "jaxin": ["plastic or wooden chair, especially one that has been mass-produced"],
+    "jegen": ["hook"],
+    "jidolk": ["flower"],
+    "jikin": ["wiki"],
+    "jizlet": ["Quizlet"],
+    "kizhul": [["archaic", "kizhult"], "bee"],
+    "kizhult": ["bee", "harvester", ["see also", "znolc"], "storyteller"],
+    "kolthic": ["the joy and exhaustion that comes after final exams are over"],
+    "kuhc": ["letter"],
+    "kyfik": ["arm"],
+    "mizizlat": ["wheelbarrow", "cart", "trolley", "pram"],
+    "molm": ["tribe"],
+    "molmelc": ["roof", "shelter"],
+    "muhc": ["that which cities, forests, and hives have in common, a community"],
+    "muhzl": [["archaic", "muhc"], "that which cities, forests, and hives have in common, a community"],
+    "nasham": ["wave"],
+    "nyzlan": ["snail"],
+    "sekelt": ["valley"],
+    "shelsheln": ["river", "pragmatics"],
+    "shnuhk": ["lip"],
+    "shuhzhik": ["tear", "drop"],
+    "slik": ["thumbs-up"],
+    "slin": ["fight"],
+    "slingyn": ["warrior", "Klingon"],
+    "slysyc": ["snake"],
+    "snolzem": ["knot"],
+    "snym": ["cloud"],
+    "sulfekahslifazhislitikesixahsloldolshyc": ["cookie"],
+    "sulslem": ["nectar"],
+    "tah": [["archaic", "tan"], "honey", "vomit", "language"],
+    "tan": ["honey", "vomit", "language"],
+    "tahzluv": [["archaic", "tanzlulc"], "utterance", "short speech", "sentence", "dollop of honey"],
+    "tanzlulc": ["utterance", "short speech", "sentence", "dollop of honey"],
+    "telzlnoln": ["rain"],
+    "tetan": ["flying snail"],
+    "tezet": ["lightning"],
+    "thefam": ["stone", "writing", "orthography"],
+    "thefahm": [["archaic", "thefam"], "stone"],
+    "thefnolm": ["bear"],
+    "thethat": ["wind"],
+    "thneduht": ["silence", "ominous stillness", "void"],
+    "thnuhduhk": ["elephant"],
+    "tolmolm": ["slope"],
+    "tuln": ["eye"],
+    "tytyt": ["clover"],
+    "vith": [["archaic", "vithit"], "bird"],
+    "vithit": ["bird", "overseer"],
+    "vmelfelc": ["wisdom", "silliness"],
+    "vmyn": ["mouth"],
+    "voljam": ["ear"],
+    "xamatin": ["the frustration of realizing that one has inadvertently invalidated or undone an accomplisment"],
+    "xataxym": ["hole", "pit", "ditch"],
+    "xelteln": ["cliff"],
+    "xulkat": ["fun", "grammar", "climbing", "the enjoyment of challenges"],
+    "zatenym": ["troop", "group", "platoon"],
+    "zatheln": ["spider", "silkworm"],
+    "zhasik": ["sky", "canopy of a forest", "ceiling of a cave (but not of a house)"],
+    "zhetam": ["pollen", "semen"],
+    "zhimahn": [["archaic", "zhiman"], "feather"],
+    "zhiman": ["feather", "long lustrous hair"],
+    "zhizlik": ["fish"],
+    "zhuhzhim": ["dust", "a floating particle; something that settles down or doesn't"],
+    "zlislym": ["skeleton"],
+    "zlnanic": ["chameleon"],
+    "zlolfit": ["wing"],
+    "zlolzhit": ["poop (thanks, Thunder's sister)"],
+    "zlulc": ["lump", "dollop (of a substance)", "instance (of something more abstract)"],
+    "znenan": ["tale", "story"],
+    "znolk": ["day"],
+    "zuhzuhmelt": ["ladle", "dipper"]
+  },
+  "number": {
+    "decimal": {
+      "dec": ["1"],
+      "fam": ["2"],
+      "guhn": ["9"],
+      "kulk": ["8"],
+      "shen": ["6"],
+      "sluht": ["4"],
+      "thnolt": ["100"],
+      "tim": ["0"],
+      "vmelt": ["1000"],
+      "vulc": ["3"],
+      "zhat": ["7"],
+      "zlik": ["5"],
+      "znek": ["10"]
+    },
+    "nolk": ["decimal point"],
+    "senary": {
+      "dolk": ["1"],
+      "felt": ["2"],
+      "folt": ["36"],
+      "slolc": ["4"],
+      "tyt": ["0"],
+      "vyk": ["3"],
+      "xek": ["6"],
+      "zhelt": ["216"],
+      "zlelc": ["5"]
+    }
+  },
+  "particle": {
+    "cehk": ["introduces a new word"],
+    "cehsh": [["archaic", "cehk"], "introduces a new word"],
+    "con": ["transliteration, may be phonotactically invalid"],
+    "don": ["sounds like the phoneme sequence", ["example", "don zelzeln = sound of a bee"]],
+    "kut": ["feels like the phoneme sequence", ["example", "kut xigeldifigeldit = all over the place, higgledy-piggledy"]],
+    "om": ["carries conjugations for con, don, kut, shehn, and tahn", ["example", "kut xigeldifigeldit omum culjimah = the ants are all over the place / disorganised"]],
+    "shehn": ["feels like", ["example", "shehn simem = the feeling of peace"]],
+    "slah": ["introduces a new sense"],
+    "tahn": ["sounds like", ["example", "tahn divolm = the sound of thunder"]],
+    "thom": ["links word with new definition"],
+    "xo": ["marks an archaic word"]
+  },
+  "post": {
+    "navnyc": ["upon"],
+    "sholmak": ["between"],
+    "sno": ["in the time of", "during"]
+  },
+  "pronoun": {
+    "seh": ["1st person animate"],
+    "sehth": [["archaic", "seh"], "1st person animate"],
+    "theh": ["1st person inanimate"],
+    "thehs": [["archaic", "theh"], "1st person inanimate"],
+    "thu": ["1st person vegetal"],
+    "thuzl": [["archaic", "thu"], "1st person vegetal"],
+    "vo": ["2nd person vegetal"],
+    "vos": [["archaic", "vo"], "2nd person vegetal"],
+    "vu": ["2nd person inanimate"],
+    "vuzh": [["archaic", "vu"], "2nd person inanimate"],
+    "zeh": ["3rd person vegetal"],
+    "zehzh": [["archaic", "zeh"], "3rd person vegetal"],
+    "zhu": ["2nd person animate"],
+    "zhuv": [["archaic", "zhu"], "2nd person animate"],
+    "zlo": ["3rd person animate"],
+    "zloz": [["archaic", "zlo"], "3rd person animate"],
+    "zo": ["3rd person inanimate"],
+    "zozl": [["archaic", "zo"], "3rd person inanimate"]
+  },
+  "suffix": {
+    "aspect": {
+      "da": ["perfective"],
+      "eht": ["habitual"],
+      "fe": [["archaic", "feh"], "durative"],
+      "feh": ["durative"],
+      "sho": ["continuous"],
+      "um": ["gnomic"]
+    },
+    "case": {
+      "gu": ["instrmental I -- tools that fit in the hand"],
+      "kahm": ["comitative"],
+      "thot": ["recipient/beneficiary"],
+      "vah": ["instrumental II -- tools too big to hold in the hand, and intangible tools like software"],
+      "vmeh": ["genitive"],
+      "zheh": ["focus/source"]
+    },
+    "noun>adj": {
+      "meht": ["having a surfeit or plenitude of something", ["example", "thefam (stone, rock) > thefammeht (stony, rocky)"]]
+    },
+    "noun>noun": {
+      "ah": ["plural"],
+      "kah": ["energetic", "coffee"],
+      "kahf": [["archaic", "kah"], "energetic", "coffee"],
+      "mon": ["collective noun", "a group of"],
+      "so": ["having-one", "an entity having the thing referred to by the stem", ["example", "zhiman (feather) > zhimanso (feathered one)"]],
+      "zhum": ["pet"]
+    },
+    "noun>verb": {
+      "zhut": ["cause to be"]
+    },
+    "valency": {
+      "sun": ["passive voice"]
+    },
+    "verb>noun": {
+      "noc": ["the end state or product of an action"]
+    }
+  },
+  "verb": {
+    "impersonal": {
+      "kem": ["shake"]
+    },
+    "intransitive": {
+      "doln": ["make a loud, deep sound", "grumble", "rumble"],
+      "kem": ["vibrate"],
+      "muhshim": ["be cool", "be awesome"],
+      "sesuht": ["sing Let It Go"],
+      "thek": ["scribble", "scrawl"],
+      "tho": [["archaic", "thek"], "scribble", "scrawl"],
+      "vaslnulm": ["cease"],
+      "xaslem": ["glide"],
+      "xaxat": ["laugh"],
+      "zamen": ["hope"],
+      "zhuhzhuhm": ["fly"],
+      "znim": ["fall (unconstrained motion under influence of gravity)"]
+    },
+    "transitive": {
+      "cejym": ["obsessively organize minute details"],
+      "delgem": ["select", "curate", "anthologize"],
+      "fmyvu": [["archaic", "fmyvuhk"], "make", "create"],
+      "fmyvuhk": ["make", "create"],
+      "gulslim": ["nourish"],
+      "kem": ["shake", "cause to vibrate"],
+      "ma": [["archaic", "man"], "thank"],
+      "man": ["thank"],
+      "nanym": ["dispatch, especially for military purposes"],
+      "sesuht": ["sing boisterously, especially music that is disliked by others"],
+      "shasholc": ["partake"],
+      "shelgin": ["perceive"],
+      "slin": ["fight"],
+      "tah": [["archaic", "tan"], "speak", "vomit"],
+      "tan": ["speak", "vomit"],
+      "thek": ["scribble", "scrawl"],
+      "tho": [["archaic", "thek"], "scribble", "scrawl"],
+      "vaslnulm": ["cease"],
+      "xanoln": ["greet"],
+      "xaslem": ["transition smoothly"],
+      "zan": ["be", "is"],
+      "znolc": ["gather", "collect"],
+      "znols": [["archaic", "znolc"], "gather", "collect"]
+    }
+  }
+};
+var getProps = function(w) {
+  var r = {"defs": []};
+  for (var i = 0; i < w.length; i++) {
+    if (w[i].constructor == Array) {
+      r[w[i][0]] = w[i].slice(1);
+    } else {
+      r.defs.push(w[i]);
+    }
+  }
+  return r;
+};
+var generateAux = function(a) {
+  if (lexicon.aux.i.hasOwnProperty(a)) {
+    var w = lexicon.aux.i[a];
+    var f = getProps(w).form;
+    return ["i", "e", "a", "ul"].map(function(c) { return f[0] + c + f[1]; });
+  } else if (lexicon.aux.ol.hasOwnProperty(a)) {
+    var w = lexicon.aux.ol[a];
+    var f = getProps(w).form;
+    return ["ol", "el", "y", "uh"].map(function(c) { return f[0] + c + f[1]; });
+  } else {
+    return [];
+  }
+};
+var listWords = function() {
+  var words = [];
+  var doObj = function(obj) {
+    for (key in obj) {
+      if (obj[key].constructor == Array) {
+        if (words.indexOf(key) == -1) {
+          words.push(key);
+        }
+      } else {
+        doObj(obj[key]);
+      }
+    }
+  };
+  doObj(lexicon);
+  return words;
+};
+var listWordsProps = function() {
+  var words = {};
+  var path = [];
+  var doObj = function(obj) {
+    for (key in obj) {
+      if (obj[key].constructor == Array) {
+        var psh = getProps(obj[key]);
+        psh.path = JSON.parse(JSON.stringify(path));
+        if (words.hasOwnProperty(key)) {
+          words[key].push(psh);
+        } else {
+          words[key] = [psh];
+        }
+      } else {
+        path.push(key);
+        doObj(obj[key]);
+        path.pop();
+      }
+    }
+  };
+  doObj(lexicon);
+  return words;
+};
+var sortAlpha = function(word1, word2) {
+  var tols = function(w) {
+    var order = ["d", "g", "x", "j", "f", "v", "th", "s", "z", "sh", "zh", "sl", "zl", "m", "n", "a", "el", "e", "ol", "i", "y", "uh", "ul", "c", "t", "k", "ah", "eh", "o", "u"];
+    var i = 0;
+    var r = [];
+    var c = "";
+    while (i < w.length) {
+      c = w[i];
+      i++;
+      if (i < w.length && (w[i] == "h" || w[i] == "l")) {
+        c += w[i];
+        i++;
+      }
+      r.push(order.indexOf(c));
+    }
+    return r;
+  };
+  var l1 = tols(word1);
+  var l2 = tols(word2);
+  var i = 0;
+  while (i < l1.length && i < l2.length) {
+    if (l1[i] != l2[i]) {
+      return l1[i] - l2[i];
+    }
+    i++;
+  }
+  return l1.length - l2.length;
+};
