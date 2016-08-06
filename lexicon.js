5cb9e365ad0dad93ffbc9abf36248ac4d71c1c29
Reuben Staley <lighthousemaniac@gmail.com>
2016-08-06 15:09:54 -0600

    Made color words.

--- lexicon.js
+++ lexicon.js
@@ -153,15 +153,12 @@ var lexicon = {
     "fmolthyt": ["duty", "responsibility", "thing one ought to do", "domain", "specialization"],
     "fmolxem": ["destiny", "fate"],
     "fnadyc": ["death"],
-    "fnadycsoljem": ["clearness", "transparency"],
     "gafuht": ["brick"],
     "gajin": ["pure awe"],
     "gak": ["fun derived from a game or other entertainment"],
     "gamyk": ["tree", "one that produces"],
     "gasnoln": ["effort", "attempt", "pain"],
-    "gazhyn": ["green"],
     "geselt": ["chest", "box", "large container"],
-    "gevmulksoljem": ["black"],
     "gezhelfuln": ["one's personal potential"],
     "gigim": ["flipper", "fin"],
     "golduhn": ["three-dimensional shape or geometric object"],
@@ -224,7 +221,6 @@ var lexicon = {
     "nuldic": ["miner, usually a coal miner", "one who mines", ["gloss", "miner"]],
     "nuln": ["friend"],
     "nulvmic": ["charcoal"],
-    "nyfuln": ["purple"],
     "nyzlan": ["snail"],
     "sasoln": ["applause", "an ovation", "the rumbling praise of the crowd"],
     "saznolt": ["mud"],
@@ -234,7 +230,6 @@ var lexicon = {
     "shnuhk": ["lips"],
     "shuhzhik": ["tear", "drop"],
     "slelceln": ["lemon"],
-    "slifuhn": ["orange"],
     "slik": ["thumbs-up"],
     "slin": ["fight"],
     "slingyn": ["warrior", "Klingon"],
@@ -245,7 +240,6 @@ var lexicon = {
     "snijat": ["mountain"],
     "snolzem": ["knot"],
     "snym": ["cloud"],
-    "snymsoljem": ["white"],
     "snymsoljemthuhfam": ["Silver"],
     "soljem": ["color"],
     "soljesolm": ["chromesthesia"],
@@ -276,7 +270,6 @@ var lexicon = {
     "thnadyc": ["inspiration", "Death"],
     "thnamolk": ["pizza"],
     "thnamolkmahk": ["marinara sauce"],
-    "thnamolkmahksoljem": ["red"],
     "thnamolksoljemmahk": ["olive oil"],
     "thneduht": ["silence", "ominous stillness", "void"],
     "thnolduhn": ["zoccihedron", "a 100-sided shape"],
@@ -365,6 +358,34 @@ var lexicon = {
     "znolk": ["day"],
     "zuhzuhmelt": ["ladle", "dipper"],
     "zyzlolk": ["gas"]
+  },
+  "color": {
+    // Organised by color wheel, not alphabet
+    "primary": {
+      "gazhyn": ["green"],
+      "nyfuln": ["purple"],
+      "snitin": ["orange"]
+    },
+    "secondary": {
+      "gafuln": ["blue", ["see-also": "nyzhyn"]],
+      "nyzhyn": ["blue", ["see-also": "gafuln"]],
+      "nytin": ["red", ["see-also": "snifuln"]],
+      "snifuln": ["red", ["see-also": "nytin"]],
+      "snizhyn": ["yellow", ["see-also": "gatin"]],
+      "gatin": ["yellow", ["see-also": "snizhyn"]]
+    },
+    "tertiary": {
+      "gagafuln": ["green-blue"],
+      "nynyzhyn": ["purple-blue"],
+      "nynytin": ["purple-red"],
+      "snisnifuln": ["orange-red"],
+      "snisnizhyn": ["orange-yellow"],
+      "gagatin": ["green-yellow"]
+    },
+    "snymsoljem": ["white"],
+    "gevmulksoljem": ["black"],
+    "fnadycsoljem": ["clear", "transparant"],
+    "thnamolkmahksoljem" : ["dark red"]
   },
   "number": {
     "decimal": {
