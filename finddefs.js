35364e8c00715ad77535838f5513ae84b7eed1be
Reuben Staley <lighthousemaniac@gmail.com>
2016-10-15 18:50:22 -0600

    Change things

--- finddefs.js
+++ finddefs.js
@@ -1,21 +1,18 @@
-var findST = function(elname) {
+var findST = function(query) {
   for (var k in lexicon_flat) {
     for (var i = 0; i < lexicon_flat[k].length; i++) {
       for (var w = 0; w < lexicon_flat[k][i].defs.length; w++) {
-        if (lexicon_flat[k][i].defs[w] == elname) {
-          return "<a href=\"lexicon.html#" + k + "\">" + k + "</a>";
+        if (lexicon_flat[k][i].defs[w] == query) {
+          return k;
         }
       }
     }
   }
-  return "[none yet]";
+  return null;
 };
-var ls = document.getElementsByTagName("td");
-var n = 1;
-for (var i = 0; i < ls.length; i++) {
-  if (ls[i].id) {
-    ls[i].innerHTML = "<b>" + n + "</b><br><b>" + findST(ls[i].id) + "</b><br><span>" + ls[i].id + "</span>";
-    ls[i].className = "elem";
-    n++;
-  }
+var formatST = function(raw) {
+  if (raw != null) {
+    return "<a href=\"lexicon.html#" + k + "\">" + k + "</a>";
+  } else {
+    return "[none yet]";
 }
